fab523b87675777d2507c7200bd072e97fea013b
koztas <kiasinskiy@gmail.com>
2023-10-08 17:05:45 +0300

    header implementation

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
@@ -8,12 +19,11 @@ const H2 = styled.h2`
 	text-align: center;
 `;
 
-const Header = () => <div>Header</div>;
 const Footer = () => <div>Footer</div>;
 
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Page content</H2>
@@ -29,6 +39,6 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
