98313e8915d142ba2a6db5a88bcb7bcf8eda39a1
koztas <kiasinskiy@gmail.com>
2023-10-08 18:35:08 +0300

    creating footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = ({ className }) => {
+	const [city, setCity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q={Moscow}&appid={1a13581e26a7cbb660e489c6b84166fa}',
+		)
+			.then((res) => res.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+	return (
+		<div className={className}>
+			<div>
+				<div>Web developer blog</div>
+				<div>web@developer.ru</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleString('ru', { day: 'numeric', month: 'long' })}
+				</div>
+				<div>
+					{temperature} degrees, {weather}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	font-weight: bold;
+	background-color: #fff;
+	box-shadow: 0px 2px 17px #000;
+`;
